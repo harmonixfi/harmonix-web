2e35bfab05cd01832c62e074dbe91f6b0fff3934
leXuanNha <xuannhak13@gmail.com>
2024-12-12 22:19:22 +0700

    update og banner

--- app/constants/metadata.ts
+++ app/constants/metadata.ts
@@ -1,5 +1,5 @@
 export const METADATA = {
-  imageUrl: "https://harmonix.fi/og-banner.jpg",
+  imageUrl: "https://harmonix.fi/og-banner-v2.jpg",
   twitterSite: "@harmonixfi",
   twitterCard: "summary_large_image",
   openGraphType: "website",
