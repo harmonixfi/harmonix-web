6f38b076a3a7710eb7b5a9de492c5f363526a294
chibao99 <chibao9965@gmail.com>
2025-07-30 19:38:51 +0700

    add title

--- app/routes/terms.tsx
+++ app/routes/terms.tsx
@@ -1,5 +1,11 @@
 import TermOfServicesContent from "~/components/common/TermOfServicesContent";
 
+const metaTitle = "Harmonix | Terms of Use";
+
+export const meta: MetaFunction = () => {
+  return [{ title: metaTitle }];
+};
+
 export default function Terms() {
   return <TermOfServicesContent />;
 }
