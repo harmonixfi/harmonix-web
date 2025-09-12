b1ba87bbbf95f4dac28a10a6fc43743e48373a9d
leXuanNha <xuannhak13@gmail.com>
2025-09-12 16:31:43 +0700

    fix deploy error

--- sst.config.ts
+++ sst.config.ts
@@ -6,10 +6,10 @@ export default $config({
       name: "harmonix-remix-web",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
-      providers: {
-        aws: { region: "us-east-1" },
-        "@pulumi/aws": "7.5.0", // explicitly pin Pulumi provider version
-      },
+      // providers: {
+      //   aws: { region: "us-east-1" },
+      //   "@pulumi/aws": "7.5.0", // explicitly pin Pulumi provider version
+      // },
     };
   },
 
