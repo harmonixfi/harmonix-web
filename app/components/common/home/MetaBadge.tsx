bafa0cfd97a7fb80dfb2594945a5c4fa30407154
vuongbui97 <vuongdevtest@gmail.com>
2025-11-17 22:59:00 +0700

    Add Zenith audit

--- app/components/common/home/MetaBadge.tsx
+++ app/components/common/home/MetaBadge.tsx
@@ -1,4 +1,4 @@
-import { ShieldifyIcon, VerichainIcon } from "~/components/icons";
+import { ShieldifyIcon, VerichainIcon, ZenithIcon } from "~/components/icons";
 
 type MetaBadgeProps = {
   apy: number;
@@ -12,7 +12,9 @@ export default function MetaBadge(props: MetaBadgeProps) {
   return (
     <div className={"flex justify-center text-primary"}>
       <div
-        className={"px-6 py-4 bg-linear bg-white rounded-3xl flex flex-col md:flex-row items-center space-y-2"}
+        className={
+          "px-6 py-4 bg-linear bg-white rounded-3xl flex flex-col md:flex-row items-center space-y-2"
+        }
       >
         <div className={"flex items-baseline gap-2"}>
           <span className={"uppercase text-base"}>APY</span>
@@ -28,6 +30,7 @@ export default function MetaBadge(props: MetaBadgeProps) {
           <div>Audit</div>
           <VerichainIcon className="w-auto h-7 sm:h-9" />
           <ShieldifyIcon className="w-28 md:w-36" />
+          <ZenithIcon className="w-auto h-6 sm:h-8 pl-2 pb-1" />
         </div>
       </div>
     </div>
